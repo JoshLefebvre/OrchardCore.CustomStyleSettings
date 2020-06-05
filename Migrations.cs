d7476a200cb413c3848143a52a6d84ec5f16b7e7
Josh Lefebvre <joshlefebvre1@gmail.com>
2020-06-04 21:38:07 -0400

    Remove Securable from migratin

--- Migrations.cs
+++ Migrations.cs
@@ -23,7 +23,6 @@ namespace OrchardCore.CustomStyleSettings
             );
 
             _contentDefinitionManager.AlterTypeDefinition("CustomStyleSettings", builder => builder
-                .Securable()
                 .WithPart("CustomStyleSettingsPart", part => part.WithPosition("1"))
                 .Stereotype("CustomSettings")
             );
