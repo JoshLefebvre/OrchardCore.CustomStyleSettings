425c4f71f077cd51303365199ee4c804a2b320f5
JoshL <josh.lefebvre@accuv.com>
2020-07-01 11:25:46 -0400

    Updates to manifest

--- Manifest.cs
+++ Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.Modules.Manifest;
 [assembly: Module(
     Name = "Custom Style Settings",
     Author = "LefeWare Solutions",
-    Website = "",
+    Website = "https://LefeWareSolutions.com",
     Version = "1.0.0",
     Description = "Custom Tenant Site Style Settings",
     Category = "Settings",
